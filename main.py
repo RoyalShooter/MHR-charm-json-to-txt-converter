88087bdd2b6b79c75e1a61756f211e479492fb6c
RoyalShooter <opqxc951357@gmail.com>
2022-07-07 16:33:29 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog as fd
-from tkinter.messagebox import showinfo
 from tkinter import Text
 import os
 import json
@@ -17,7 +16,7 @@ root.resizable(False, False)
 root.geometry('600x250')
 guidetexteng="""
 Step one: Choose the language you would like to output
-Step two: Click on select Json to convert your Charm json to text file
+Step two: Click on select Json to convert your Charm json to txt file
 
 !! When the converter finish it will save in the same location
    with the same name in txt format !!
@@ -30,6 +29,16 @@ guidetextzht = """
 
 !! 當轉換器完成以後將會以txt格式自動把Json在同一路徑儲存成一樣的檔案名稱 !!
 
+
+"""
+
+guidetextjpn="""
+ステップ1: 使用する言語を選択してください。
+ステップ2: 「Jsonファイルを選択」をクリックし、json形式のお守りデータを選択したらtxt形式に変換されます。
+
+!! 変換されたお守りデータは変換前と同じ場所に同じ名前で保存されます。!!
+   
+~ 日本語に翻訳したL4Mon69感謝します ~
 
 """
 
@@ -66,14 +75,26 @@ def menu_updated_zht():
     text_box.insert('end', guidetextzht)
     text_box.config(state='disabled')
 
+def menu_updated_jpn():
+    global userleng
+    userleng = "jpn"
+    mb.configure(text="日本語")
+    open_button.configure(text="Jsonファイルを選択")
+    text_box.config(state='normal')
+    text_box.delete(1.0, 'end')
+    text_box.insert('end', guidetextjpn)
+    text_box.config(state='disabled')
+
 def conver_json_to_string(userfilepath):
     # start script
-    print("path = " + userfilepath)
+    print("Read file path = " + userfilepath)
     datafile = open(userfilepath)
     if userleng == "zht":
         skilljson = open("leng/skills_zht.json" , encoding="utf8")
     elif userleng == "eng":
         skilljson = open("leng/skills_eng.json" , encoding="utf8")
+    elif userleng == "jpn":
+        skilljson = open("leng/skills_jpn.json" , encoding="utf8")
 
 
     data = json.load(datafile)
@@ -97,13 +118,6 @@ def conver_json_to_string(userfilepath):
         f.write(out)
         print(out)
 
-        """
-        print(i)
-        print(i["Rarity"])
-        print(i["SkillLevels"])
-        print(i["Skills"])
-        print(i["Slots"])
-        """
     f.close()
     datafile.close()
     skilljson.close()
@@ -119,6 +133,7 @@ mb.menu = Menu(mb, tearoff=0)
 mb['menu'] = mb.menu
 mb.menu.add_command(label="English", command=menu_updated_eng)
 mb.menu.add_command(label="中文(zh-CHT)", command=menu_updated_zht)
+mb.menu.add_command(label="日本語", command=menu_updated_jpn)
 mb.pack()
 open_button = ttk.Button(
     root,
@@ -131,7 +146,9 @@ text_box.pack(expand=True)
 text_box.insert('end', guidetexteng)
 text_box.config(state='disabled')
 
+#set default to english
+mb.menu.invoke(0)
+
 #start GUI
 root.mainloop()
-
 
